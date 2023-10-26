25d6e7b2d4b6506f49e4cb7c29aa20b9c56892ae
Stephon Tonge <stephon.tonge75@gmail.com>
2023-10-26 16:52:20 -0500

    added sample data inserts

--- database.sql
+++ database.sql
@@ -4,3 +4,12 @@ CREATE TABLE TODO List (
     "Completed" BOOLEAN DEFAULT FALSE
     "Not Completed" BOOLEAN DEFAULT TRUE
 );
+
+INSERT INTO TODOList ("Task", "Completed")
+VALUES 
+  ('Take out the trash', true),
+  ('Walk and feed the dog', false),
+  ('Do homework', false),
+  ('Wash the didhes', true),
+  ('Have brewskis with the boys tonight', false),
+  ('Buy groceries for the month', true);
