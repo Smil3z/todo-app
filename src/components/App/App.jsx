2f35ab9c2d4d8702e76ab863ccbbc61df6073e90
Stephon Tonge <stephon.tonge75@gmail.com>
2023-10-29 18:14:19 -0500

    axios fixtures

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,38 @@
-import {useState} from 'react';
+// useEffect is a function provided by React
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App () {
-  
+  // useState creates a variable that will automatically update on
+  // the DOM (webpage).
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    // This code will run on page load (when the component displayed)
+    console.log('in useEffect');
+    // On page load, get list of todo items
+    getTodoList();
+  }, []);
+
+  // This function gets todo items from the server and updates
+  // a variable in state.
+  const getTodoList = () => {
+    console.log('getTodoList');
+    // TODO: axios.get
+    axios.get('/todolist').then((response) => {
+      // Logging the server response which is the list of todo items
+      console.log(response.data);
+      setTodoList(response.data);
+    }).catch((error) => {
+      console.log('error with GET', error);
+    })
+  }
+
   return (
     <div>
       <h1>TO DO APP</h1>
+      {/* TODO: Replace with .map, stringify is for debugging only. */}
+      {JSON.stringify(todoList)}
     </div>
   );
 
