cd16ce74193120d03cc9d6da9a6076e18bc7c493
Stephon Tonge <stephon.tonge75@gmail.com>
2023-11-03 01:45:00 -0500

    finished srver side?

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,7 +4,7 @@ const pool = require('../modules/pool.js');
 
 // GET
 router.get('/', (req, res) => {
-    console.log('GET todoist');
+    console.log('GET todolist');
     let queryText = 'SELECT * FROM "todolist" ORDER BY "task";';
     pool.query(queryText)
         .then((result) => {
@@ -17,9 +17,44 @@ router.get('/', (req, res) => {
 });
 
 // POST
-
+router.post('/', (req,res) => {
+    const todo = req.body;
+    const sqlText = `INSERT INTO todolist (task, completed)
+                    VALUES ($1, $2)`;
+    
+    pool.query(sqlText, [todo.task, todo.completed])
+        .then((result) => {
+            console.log(`added task to database`, todo);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`error with database query ${sqlText}`, error);
+            res.sendStatus(500);
+        })
+});
 // PUT
-
+router.put('/:id', (req, res) => {
+    console.log('PUT /todolist', req.params, req.body);
+    let queryText =`
+        UPDATE "todolist" SET "completed" = true
+        WHERE "id"
+    `;
+    pool.query(queryText, [req.body.completed])
+    .then((result) => {
+        res.sendStatus(200);
+    }) .catch((error) => {
+        console.log('error in PUT /todolist', error)
+        res.sendStatus(500);
+    });
+});
 // DELETE
+router.delete(',:id', (req, res) => {
+    console.log('req.params', req.params);
+    let queryText = 'DELETE FROM "todolist" where "id"';
+    pool.query(queryText, [req.params.id])
+        .then((reult) => {
+            res.sendStatus(500);
+        })
+});
 
 module.exports = router;
