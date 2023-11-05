4e220092eaf690a9fba5636556a9c7b556a12ba1
Stephon Tonge <stephon.tonge75@gmail.com>
2023-11-05 01:18:49 -0600

    trying to intergrate everything

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -2,6 +2,37 @@ import { useState } from "react";
 import axios from "axios";
 
 function TodoForm(props) {
+    const [task, setTask] = useState('');
+    const [completed, setCompleted] = useState('');
 
-    
+    const sendTodolistToServer = (event) => {
+        event.preventDefault();
+
+        console.log('sendTodolistToServer', task);
+        axios.post('/todo', {
+            task: task,
+            completed: cpmpleted,
+        }).then((responde) => {
+            console.log(response.data);
+            setTask('');
+            markCompleted('');
+            props.getTodolistList();
+        }).catch((error) => {
+            console.error(error);
+            alert('Something went wrong!!');
+        })
+    }
+    return(
+        <>
+            <h2>Todo ListForm</h2>
+            <form onSubmit={sendTodolistToServer}>
+                Task: <input value={task} onChange={(e) => setTask(e.target.value)} />
+                    <p>{task}</p>
+                    <br />
+                Completed <input value={completed} onChange={(e) => markCompleted(e.target.value)}></input>
+                <p>{completed}</p>
+                <button>Submit</button>
+            </form>
+        </>
+    )
 }
