27b669d7279bd1fb7e87221911d5990ba6975c59
Stephon Tonge <stephon.tonge75@gmail.com>
2023-11-06 22:12:52 -0600

    TodoItem.jsx

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -3,7 +3,7 @@ import { Grid } from "@mui/material";
 import './TodoItem.css';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Card from "@mui/material";
+import { Card } from "@mui/material";
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
@@ -29,20 +29,22 @@ function TodoItem(props) {
     }
     return (
         <Grid item xs={12} md={4}>
-            <CardContent>
-                <Typography variant='h3'>
-                    {props.todo.task}
-                </Typography>
-                {
-                    props.todo.completed === false && (
-                        <p>Not Complete</p>
-                    )
-                    }
-            </CardContent>
-            <CardActions sx={{ padding:'20px', float:'right'}}>
-                <Button variant="outlined" onClick={clickHandler}>Delete</Button>
-                <Button variant='contained' onClick={markComplete}>Completed</Button>
-            </CardActions>
+            <Card>
+                <CardContent>
+                <CardActions sx={{ padding:'20px', float:'right'}}>
+                    <Button variant="outlined" color="error" onClick={clickHandler}>Delete</Button>
+                    <Button variant='contained' onClick={markComplete}>Completed</Button>
+                </CardActions>
+                    <Typography variant='h3'>
+                        <p>{props.todo.task}</p>
+                    </Typography>
+                    {
+                        props.todo.completed === false && (
+                            <p>Not Complete</p>
+                        )
+                        }
+                </CardContent>
+            </Card>
         </Grid>
 
     )
